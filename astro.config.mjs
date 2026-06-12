80c48976e384b75c23b69c3d2c561edb23357fb8
Gianfranco Palumbo <gianpa@gmail.com>
2026-06-12 17:36:04 +0200

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,26 @@
-import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
-import tailwindcss from "@tailwindcss/vite";
 import { unified } from '@astrojs/markdown-remark';
-import { defineConfig } from "astro/config";
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
-import { remarkExcerpt } from "./remark-excerpt.mjs";
+import { remarkExcerpt } from './remark-excerpt.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gian.cool",
+  site: 'https://gian.cool',
   integrations: [
     mdx({
       processor: unified({ remarkPlugins: [remarkExcerpt] }),
     }),
     react(),
+    sitemap(),
   ],
   markdown: {
     processor: unified({
-     remarkPlugins: [remarkExcerpt],
-    })
+      remarkPlugins: [remarkExcerpt],
+    }),
   },
   vite: {
     plugins: [tailwindcss()],
